6656518f553e18c0daf8cbbeb06ea1514eb4866c
kurehajime <xiidec@gmail.com>
2023-02-08 21:13:53 +0900

    移動

--- gpt_index/gpt.py
+++ gpt_index/gpt.py
@@ -1,5 +1,5 @@
 from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 documents = SimpleDirectoryReader('data').load_data()
 index = GPTSimpleVectorIndex(documents)
-response = index.query("世界を変える人が四角い穴に打ち込むものは何?")
+response = index.query("クレイジーな人を無視したら、世界は変わる?")
 print(response)
